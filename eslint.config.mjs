bc61f4f375a50fbc63ba9b82051e9e07588321ce
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-11-12 22:15:02 +0700

    refactor: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,20 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import js from "@eslint/js"
+import globals from "globals"
+import * as tseslint from "typescript-eslint"
+import prettier from "eslint-config-prettier"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
-  },
-  tseslint.configs.recommended,
-  eslintConfigPrettier.config.recommended,
-  {
-    rules: {
-      indent: ['error', 4],
-      'linebreak-style': 'off',
-      quotes: ['error', 'double'],
-      semi: ['error', 'never'],
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        languageOptions: { globals: globals.node },
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+        rules: {
+            indent: ["error", 4],
+            "linebreak-style": "off",
+            quotes: ["error", "double"],
+            semi: ["error", "never"],
+            "@typescript-eslint/no-explicit-any": "error",
+        },
     },
-  },
-]);
+])
