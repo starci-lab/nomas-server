a0ec6acbff029b4f8da84e51845010910ef67c3d
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-11-30 19:16:31 +0700

    chore(deploy): add debugging step for environment variables in GitHub Actions workflow to assist in troubleshooting

--- apps/nomas-colyseus/src/main.ts
+++ apps/nomas-colyseus/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from "./app"
 import "dotenv/config"
 import "@modules/sentry/instrument"
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston"
+import { envConfig } from "@modules/env"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
+    console.log("envConfig().kafka", envConfig().kafka)
     globalThis.__APP__ = app
     await app.listen(process.env.PORT ?? 3000)
 }
