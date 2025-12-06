9c52d8afea9e06dcb7ff76cb45048e56f76a2286
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-12-06 21:10:14 +0700

    refactor(main): remove console log for envConfig in bootstrap function

--- apps/nomas-colyseus/src/main.ts
+++ apps/nomas-colyseus/src/main.ts
@@ -3,12 +3,10 @@ import { AppModule } from "./app"
 import "dotenv/config"
 import "@modules/sentry/instrument"
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston"
-import { envConfig } from "@modules/env"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
-    console.log("envConfig().kafka", envConfig().kafka)
     globalThis.__APP__ = app
     app.enableCors({
         origin: true, // Allow all origins in development (change to specific origins in production)
